8a54bb9f52ee22fb66d3a4628f788108c19b73f8
Slowpoke <arseniy.budesko@gmail.com>
2024-10-08 22:07:12 +0300

    Перенес конструктор user.cpp в user.h

--- User.cpp
+++
@@ -1,4 +0,0 @@
-#include "User.h"
-
-User::User(const std::string& login, const std::string& password, const std::string& name)
-    : login(login), password(password), name(name) {}
--- User.h
+++ User.h
@@ -8,5 +8,5 @@ public:
 
     User() = default;
 
-    User(const std::string& login, const std::string& password, const std::string& name);
+    User(const std::string& login, const std::string& password, const std::string& name) : login(login), password(password), name(name) {}
 };
