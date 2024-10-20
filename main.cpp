02f5243d2babadf3cbc219de9436d8bddbbbedc5
folkus <folkus369@gmail.com>
2024-10-20 10:42:28 +0400

    добавил глобальные сообщения

--- main.cpp
+++ main.cpp
@@ -5,16 +5,18 @@
 #include "LMStorage.h"
 #include "UserStorage.h"
 #include "LocalMessage.h"
+#include "GlobalMessage.h"
 using namespace std;
 
 int main() 
 {
 	setlocale(LC_ALL, "ru");
 	UserStorage userstorage; // классы чисто для взаимодействия
-	LMStorage lmstorage;
+	LMStorage lmstorage; // хранилище локальных сообщений
+	GlobalMessage globalMessage(&userstorage); // хранилище глобальных сообщений
 	//Chat chat;
 
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i < 3; i++)
 	{
 		string name;
 		string password;
@@ -28,15 +30,33 @@ int main()
 		}
 		cout << "succses" << endl;
 	}
-	LocalMessage lm(userstorage[0], userstorage[1]);
+	// локальный чат
+	LocalMessage lm(userstorage[0], userstorage[1]); // создаем локальный чат между двумя пользователями
 	string m;
 	cout << "введи сообщение от первого пользователя: ";
-	cin >> m;
-	lm.SendMessage(userstorage[0]->get_login(), m);
+	cin >> m; 
+	lm.SendMessage(userstorage[0]->get_login(), m); // отправляем сообщение от имени введенного пользователя
 	cout << "введи сообщение от второго пользователя: ";
 	cin >> m;
-	lm.SendMessage(userstorage[1]->get_login(), m);
-	lm.PrintAllMessage();
-	lmstorage.addLM(&lm);
+	lm.SendMessage(userstorage[1]->get_login(), m); // отправляем сообщение от имени введенного пользователя
+	lm.PrintAllMessage(); // выводим все сообщения в этом чате
+	lmstorage.addLM(&lm); // добавляем эту переписку в хранилище локальных переписок
+	
+	// глобальный чат
+	cout << "введи сообщение от первого пользователя: ";
+	cin >> m;
+	globalMessage.SendMessage(userstorage[0]->get_login(), m);
+
+	cout << "введи сообщение от второго пользователя: ";
+	cin >> m;
+	globalMessage.SendMessage(userstorage[1]->get_login(), m);
+
+	cout << "введи сообщение от третьего пользователя: ";
+	cin >> m;
+	globalMessage.SendMessage(userstorage[2]->get_login(), m);
+
+	globalMessage.PrintAll();
+
+
 	cout << "end" << endl;
 }
